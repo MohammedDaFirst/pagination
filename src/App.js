3c25e199592c6a91c6bea078f774cb7dfc402031
lucian <luciferiizero@gmail.com>
2022-07-06 17:52:56 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './useFetch'
+import Follower from './Follower'
+function App() {
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page])
+  }, [loading, page, data])
+
+  const handlePage = (index) => {
+    setPage(index);
+  }
+  return <>
+    <main>
+      <div className="section-title">
+        <h1>{loading ? 'Loading...' : 'Pagination'}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {followers.map(follower => {
+            return <Follower key={follower.id} {...follower} />
+          })}
+        </div>
+        {!loading && <div className="btn-container">
+          {1 <= page && <button className="prev-btn" onClick={() => { handlePage(page - 1); }}>prev</button>}
+          {data.map((item, index) => {
+            return <button className={`page-btn ${index === page && 'active-btn'}`} onClick={() => {
+              handlePage(index);
+            }}>
+              {index + 1}
+            </button>
+          })}
+          {followers.length >= page && <button className="next-btn" onClick={() => { handlePage(page + 1); }}>next</button>}
+
+        </div>}
+
+      </section>
+    </main></>
+}
+
+export default App
